a44fe5eb49fdcd0f546ff32b66177a3909e96da9
Pedro Mass <pedro.j.mass@gmail.com>
2015-08-09 08:37:01 -0400

    Move the user stuff in the router to a controller.

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,15 +1,6 @@
 module.exports = function(app) {
 	var NoteController = require('../controllers/notes.server.controller');
-
-	var jwt = require('express-jwt');
-	var tokenSecret = 'Secret';
-
-	// locks down the application
-	var auth = jwt({
-		secret: tokenSecret,
-		userProperty: 'payload'
-	});
-
+	var UserController = require('../controllers/users.server.controller');
 
 	app.route('/api/notes')
 		.get(NoteController.list)
@@ -25,56 +16,12 @@ module.exports = function(app) {
 
 	app.param('noteId', NoteController.noteById);
 
-
-	var passport = require('passport');
-	var User = require('../models/user');
-
 	app.route('/api/register')
-		.post(function(req, res, next) {
-			if (!req.body.username || !req.body.password) {
-				return res.status(400).json({
-					message: 'Please fill out all fileds'
-				});
-			}
-
-			var user = new User();
-			user.username = req.body.username;
-			user.setPassword(req.body.password);
-
-			user.save(function(err) {
-				if (err) {
-					return next(err);
-				}
-
-				return res.json({
-					token: user.generateJWT()
-				});
-			});
-		});
+		.post(UserController.register);
 	;
 
 	app.route('/api/login')
-		.post(function(req, res, next) {
-			if (!req.body.username || !req.body.password) {
-				return res.status(400).json({
-					message: 'Please fill out all fileds'
-				});
-			}
-
-			passport.authenticate('local', function(err, user, info) {
-				if (err) {
-					return next(err);
-				}
-
-				if (user) {
-					return res.json({
-						token: user.generateJWT()
-					})
-				} else {
-					return res.status(401).json(info);
-				}
-			})(req, res, next);
-		});
+		.post(UserController.login);
 	;
 
 	// if none of the other routes are matched, default route
